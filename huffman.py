5bee1612bee41b854a51ab68f1253bcd78c05405
Dillon Fagan <dillonfagan@gmail.com>
2017-11-06 18:12:14 -0500

    Added invariant documentation for assignment submission

--- huffman.py
+++ huffman.py
@@ -10,7 +10,7 @@ except:
 
 '''
 Traverses a given huffman tree and returns the binary
-code dictionary.
+code dictionary containing a binary code for each unique character.
 '''
 def traverse(tree, bincode = '', codes = {}):
     for (i, subtree) in enumerate(tree):
@@ -38,23 +38,26 @@ def code(msg):
     # take count of unique characters in msg
     for c in msg:
         if c not in chars:
+            # Invariant (init): init for each unique character
             chars[c] = 1
         else:
-            # Invariant (maint): increment occurrences for each pass
+            # Invariant (maint): increment occurrences of character for each pass
             chars[c] += 1
 
     # Invariant (init): binary tree representation of msg
     tree = []
 
-    # build the initial forest
+    # Invariant (maint): build the initial forest with each character as a tree
     for (char, count) in chars.items():
-        x = (count, char) # (frequency, subtree)
+        x = (count, char) # tuple(frequency, subtree)
         tree.append(x)
 
-    # Invariant (maint): sort forest by weight of each subtree
+    # Invariant (maint): sort forest by weight of each tree
+    # Should be able to get the two subtrees with the lowest weight
+    #   if the list is sorted.
     tree.sort(key = lambda s: s[0])
 
-    # merge the forest into two subtrees
+    # Invariant (maint): merge the forest into two subtrees
     while len(tree) > 2:
         # get subtree with lowest weight
         s = tree[0]
@@ -70,11 +73,16 @@ def code(msg):
         tree.append((weight, subtree))
         tree.sort(key = lambda s: s[0])
 
-    # build codebook
+    # Invariant (init): build codebook for encoding message
+    # Each character in the message should have been assigned a code
+    #   in the codebook.
     codes = traverse(tree)
 
-    # assemble binary string from codebook
+    # Invariant (init): empty string to hold the encoded message
     string = ''
+
+    # Invariant (maint): append the code of each character to the encoded
+    #   message using the codebook
     for c in msg:
         string += codes[c]
 
@@ -82,30 +90,45 @@ def code(msg):
 
 
 def decode(string, decoderRing):
+    # Invariant (init): will hold decoded message
     msg = ''
+
+    # Invariant (init): codebook for bit codes
     codes = traverse(decoderRing)
+
+    # Invariant (init): temporary storage to examine bit combinations in order
+    #   to identify a character that matches the bitcode from codebook
     bincode = ''
 
     for bit in string:
+        # Invariant (maint): append next bit to bincode and check whether
+        #   there is a character assigned to a bitcode equal to bincode
         bincode += bit
         if bincode in codes.values():
             for (char, code) in codes.items():
                 if code == bincode:
                     msg += char
-            bincode = ''
+            bincode = '' # (maint): reset
 
     return msg
 
 
 def compress(msg):
+    # Invariant (init): string of bits (the encoded message)
+    # Invariant (init): huffman tree to later decompress message
     string, tree = code(msg)
 
+    # Invariant (init): byte array to store compressed message
     bitstream = array.array('B')
 
+    # Invariant (maint): for each block of eight bits in the encoded message,
+    #   convert the eight bits into an integer and add it to bitstream. If the
+    #   final block has fewer than eight bits, the disparity (number of bits lacking)
+    #   is appended to bitstream to later assist with decompression.
     for i in range(0, len(string) - 1, 8):
-        j = i + 8
-        diff = 0
-        last = False
+        j = i + 8 # end of eight-bit block
+        diff = 0 # bits missing in last block
+        last = False # if true, exit loop
 
         if j > len(string):
             j = len(string)
@@ -132,12 +155,17 @@ def binary(s):
 
 
 def decompress(bitstream, decoderRing):
+    # cast bistream as byte array to ensure proper type
     bitstream = array.array('B', bitstream)
 
     # disparity of bits of last element relative to size of byte
     diff = bitstream.pop()
 
+    # Invariant (init): decompressed bit string
     string = ''
+
+    # Invariant (maint): convert each byte in bitstream back into blocks of bits
+    #   and append them to string.
     for (i, byte) in enumerate(bitstream):
         bits = binary(byte)
         if i < len(bitstream) - 1:
